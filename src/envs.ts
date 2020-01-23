e5f0afdb25e7603fa8c60804d9d3211bfc4cffb1
BrennerBatR <brenner.batista@outlook.com>
2020-01-23 11:52:53 -0300

    First Commit

---
+++ src/envs.ts
@@ -0,0 +1,11 @@
+import * as dotenv from 'dotenv';
+import { resolve as resolvePath } from 'path';
+
+let typeEnv = '../.env';
+switch (process.env.NODE_ENV) {
+  case 'dev': {
+    typeEnv = '.env.dev';
+    break;
+  }
+}
+dotenv.config({ path: resolvePath(process.cwd(), typeEnv) });
