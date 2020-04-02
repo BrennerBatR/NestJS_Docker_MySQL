aa90759df26fb5567a56b1660a0e8bb3e56bccb0
BrennerBatR <brenner.batista@outlook.com>
2020-04-02 16:05:05 -0300

    repairs

--- src/envs.ts
+++ src/envs.ts
@@ -1,11 +1,14 @@
-import * as dotenv from 'dotenv';
-import { resolve as resolvePath } from 'path';
+import * as dotenv from "dotenv";
+import { resolve as resolvePath } from "path";
 
-let typeEnv = '../.env';
+let typeEnv = "../.env";
 switch (process.env.NODE_ENV) {
-  case 'dev': {
-    typeEnv = '.env.dev';
+  case "dev":
+    typeEnv = ".env.dev";
     break;
-  }
+
+  default:
+    typeEnv = ".env.dev";
+    break;
 }
 dotenv.config({ path: resolvePath(process.cwd(), typeEnv) });
